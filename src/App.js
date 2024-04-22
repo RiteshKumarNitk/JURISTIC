60e1a6b07fbb1b8e4533b796244b385ac986ff65
Neeraj Rathore <neeraj8x@outlook.com>
2024-04-22 16:08:51 +0530

    package updated & disclaimer added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from "react"
 import "./App.css"
 import Pages from "./components/pages/Pages"
+import Disclaimer from "./components/disclaimer/disclaimer"
 
 function App() {
-  return <Pages />
+  const [isDisclaimerWatched, setIsDisclaimerWatched] = useState(false)
+  if (!isDisclaimerWatched) return <Disclaimer setIsDisclaimerWatched={setIsDisclaimerWatched} />
+  else return <Pages />
 }
 
 export default App
